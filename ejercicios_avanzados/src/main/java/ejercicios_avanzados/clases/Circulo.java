51d86c7b8308452c6cf937a4f5a45f85aa047d1d
root <root@kali>
2024-03-08 21:44:22 -0500

    Ejercicio 26 completado.

--- ejercicios_avanzados/src/main/java/ejercicios_avanzados/clases/Circulo.java
+++ ejercicios_avanzados/src/main/java/ejercicios_avanzados/clases/Circulo.java
@@ -1,5 +1,31 @@
 package ejercicios_avanzados.clases;
 
+// Clase Circulo con atributo radio para el Ejercicio 26
 public class Circulo {
+	// Declaramos atributos
+	private double radio;
+
+	// Getters y Setters
+	public double getRadio() {
+		return radio;
+	}
+
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+
+	// Generamos dos constructores (uno vacío y uno con el radio).
+	public Circulo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Circulo(double radio) {
+		super();
+		this.radio = radio;
+	}
+	
+	
+	
 
 }
